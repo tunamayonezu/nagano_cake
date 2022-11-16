c8d0517792783b745bf21eacc26d533fc0179d23
EC2 Default User <ec2-user@ip-172-31-46-78.ap-northeast-1.compute.internal>
2022-11-16 05:39:47 +0000

    管理・ジャンル、アイテム、オーダーアイテム

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,13 @@
+class Admin::ItemsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def deit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/deit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/deit'
+  end
   devise_for :admins
   devise_for :customers
   devise_for :users
