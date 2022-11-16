54c3a18332e1eecd069d8d0f2dd86bf3132d1170
EC2 Default User <ec2-user@ip-172-31-22-238.ap-northeast-1.compute.internal>
2022-11-16 03:42:42 +0000

    カラム修正

--- db/migrate/20221116022255_create_items.rb
+++ db/migrate/20221116022255_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
     create_table :items do |t|
       t.integer :genre_id, null: false
       t.string :name, null: false
-      t.text :introduction, null: false
+      t.text :explanation, null: false
       t.integer :price, null: false
       t.boolean :is_active, null: false
       t.timestamps
