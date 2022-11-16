9322bc748cfc4ea545930a02864fe89de7ed3cf5
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-11-16 05:41:19 +0000

    controller items orders cart_items homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :admins
   devise_for :customers
   devise_for :users
