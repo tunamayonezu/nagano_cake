fe9744fb4dceee61fea2bb68a6902c9dff216c4b
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-11-16 02:37:56 +0000

    Model&Migration add Order

---
+++ db/migrate/20221116022322_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id, null: false
+      t.integer :postage, null: false
+      t.integer :total_payment, null: false
+      t.integer :payment_method, null: false
+      t.string :postcode, null: true
+      t.text :address, null: true
+      t.string :name, null: true
+      t.integer :status, null: false,default: "0"
+      t.timestamps
+    end
+  end
+end
