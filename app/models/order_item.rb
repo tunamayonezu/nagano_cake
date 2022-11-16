55a2764a1170452074729c8e747131e0be631820
EC2 Default User <ec2-user@ip-172-31-46-78.ap-northeast-1.compute.internal>
2022-11-16 04:00:18 +0000

    _

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,5 +3,5 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
-  enum production_status: { sroduction_not_allowed: 0, waiting_for_production: 1, in_production: 2, production completed: 3 }
+  enum production_status: { sroduction_not_allowed: 0, waiting_for_production: 1, in_production: 2, production_completed: 3 }
 end
