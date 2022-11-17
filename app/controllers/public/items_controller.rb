0488f05ab2aafff5cd1a1b610e4d8e41300a5425
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-11-17 04:05:29 +0000

    item & order 追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @genres = Genre.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+    @cart_item = CartItem.new
   end
 end
