4d849d3df1482e9844d687de6ee2198c6f3f85a7
EC2 Default User <ec2-user@ip-172-31-46-78.ap-northeast-1.compute.internal>
2022-11-20 06:07:24 +0000

    オーダ詳細追記

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,7 +3,7 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
-  enum production_status: { sroduction_not_allowed: 0, waiting_for_production: 1, in_production: 2, production_completed: 3 }
+  enum production_status: { 着手不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3  }
 
   ###############
 
