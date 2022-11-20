13e17adb1486bd104dddf48ed0301b467e01916c
RanSaitou <112914054+DMMwcRen753@users.noreply.github.com>
2022-11-20 08:23:49 +0000

    OrderItemController

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrderItemsController < ApplicationController
 
- def update
+  def update
     @order = Order.find_by(params[:id])
     @order_item = OrderItem.find(params[:id])
     @order_items = @order.order_items.all
@@ -9,19 +9,20 @@ class Admin::OrderItemsController < ApplicationController
     if @order_item.update(order_item_params)
 
      #@order.order_itmes.any?{|order_item| order_itme.production_status == "製作中"}
-        @order.update(status: 2)
-        if @order_item.production_status == "製作中"
-          @order_items.each do |order_item|
-            if order_item.production_status != "製作完了"
+      @order.update(status: 2)
+      if @order_item.production_status == "製作中"
+        @order_items.each do |order_item|
+          if order_item.production_status != "製作完了"
             is_updated = false
-      end
+          end
       #if @order.order_itmes.all?{|order_itme| order_itme.production_status == "製作完了"}
         #@order.update(status: "発送準備中")
+        end
+        @order.update(status: 3 ) if is_updated
       end
-      @order.update(status: 3 ) if is_updated
-    end
 
       redirect_to admin_order_path(@order_itme.order)
+    end
   end
 
 
@@ -34,4 +35,3 @@ class Admin::OrderItemsController < ApplicationController
 end
 
 
-end
