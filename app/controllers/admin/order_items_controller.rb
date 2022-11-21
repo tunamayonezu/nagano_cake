cc9ad97d91431d7745d4cbf648cfc56c34db0709
EC2 Default User <ec2-user@ip-172-31-22-238.ap-northeast-1.compute.internal>
2022-11-21 03:21:31 +0000

    注文・製作ステータス修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,17 +1,16 @@
 class Admin::OrderItemsController < ApplicationController
 
   def update
-    @order = Order.find_by(params[:id])#id以外のカラムを検索している　モデルの中かな
-    @order_item = OrderItem.find(params[:order_item][:order_item_id])#ビューのhidden_fieldを使用するのにorder_itemとそのidを定義している
-    #@order_items = @order.order_items.all
+      @order_item = OrderItem.find(params[:id])
+      @order = @order_item.order
 
     if @order_item.update(order_item_params)
-      #params[:order_item][:production_status] == "in_production"#英語に変える 注文した商品の製作ステータスが、もしも製作中だったら
+      if params[:order_item][:production_status] == "in_production"#英語に変える 注文した商品の製作ステータスが、もしも製作中だったら
         @order.update(status: 2)
         #注文ステータスを製作中に変えてかsら
         @order_item.update(order_item_params)#注文した商品(製作ステータス)を更新をする
+      end
     end
-
     if @order.order_items.count == @order.order_items.where(production_status: "production_complete").count
       #もしも注文した商品の数と、製作完了した商品の数が一致したら
        @order.update(status: 3)#注文ステータスを発送準備中に変える
