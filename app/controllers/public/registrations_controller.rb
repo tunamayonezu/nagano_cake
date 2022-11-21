77bae1da7d0f70b56ee7f604d2e41f3df87d2f3d
EC2 Default User <ec2-user@ip-172-31-22-238.ap-northeast-1.compute.internal>
2022-11-21 03:57:12 +0000

    ログイン後TOPページにいく

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def after_sign_up_path_for(resource)
+       mypage_path
+  end
+
+
 end
